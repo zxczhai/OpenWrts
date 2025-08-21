4a0029a1b8612e32ffd6c2548a4e4b1dc66287c6
CongZhai <89773545+zxczhai@users.noreply.github.com>
2025-08-22 00:24:50 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@
 sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
 
 # 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
+#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
