f659ad1c5c151af1317710e6d5003431981a61d7
CongZhai <89773545+zxczhai@users.noreply.github.com>
2025-08-22 00:44:12 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,8 +2,9 @@
 # Modify default system settings
 
 # 暂时回滚
-sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
-
+#sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
+sed -i '/^src-git luci/s/^/#/' feeds.conf.default
+echo 'src-git luci https://github.com/coolsnowwolf/luci' >> feeds.conf.default
 # 修改默认IP为192.168.10.1
 #sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 
